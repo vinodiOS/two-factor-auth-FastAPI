c8b85c552280892baa1ac1651e666c9d1229b78c
vinodiOS <vinod.jagtap@hotmail.com>
2022-03-31 15:01:12 +0530

    sqlite update

--- database/session.py
+++ database/session.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./two_fa_app.db"
 engine = create_engine(
      SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
